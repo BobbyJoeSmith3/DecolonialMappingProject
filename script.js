10c0fcc32f7b56fb97b352f99f320866723929dc
Bobby Joe Smith III <bsmith07@risd.edu>
2019-12-06 06:54:12 -0500

    Center Rhode Island on the Map

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ const mappa = new Mappa("Leaflet");
 
 // Put all of the map options in a single object
 const options = {
-    lat: 0,
-    lng: 0,
-    zoom: 4,
+    // Center of Rhode Island
+    lat: 41.718114, //0
+    lng: -71.572221, //0
+    zoom: 9, //4
     // style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
     style: "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png" // Dark map style
 }
@@ -37,8 +38,15 @@ function setup() {
 
     // Load the Data
     meteorites = loadTable('Meteorite_Landings.csv', 'csv', 'header');
+    // Load the Data
+    mills = loadTable('Mills.csv', 'csv', 'header');
+    // Load the Data
+    monuments = loadTable('Monuments.csv', 'csv', 'header');
     // Only redraw the meteorites when the map position changes and not every frame
-    myMap.onChange(drawMeteorites);
+    // myMap.onChange(drawMeteorites);
+
+    // Only redraw the meteorites when the map position changes and not every frame
+    myMap.onChange(drawPoint);
 
     // Add a color to our ellipse
     fill(200, 100, 100);
@@ -51,10 +59,35 @@ function drawPoint() {
     // Clear the canvas
     clear();
 
-    // Get the canvas position for the latitude and longitude of Nigeria
-    const nigeria = myMap.latLngToPixel(11.396396, 5.076543);
+    // // Get the canvas position for the latitude and longitude of Nigeria
+    // const nigeria = myMap.latLngToPixel(11.396396, 5.076543);
+    // // Using that position, draw an ellipse
+    // ellipse(nigeria.x, nigeria.y, 20, 20);
+
+    // Get the canvas position for the latitude and longitude of Providence Rhode Island
+    const providence = myMap.latLngToPixel(41.825995, -71.407743);
     // Using that position, draw an ellipse
-    ellipse(nigeria.x, nigeria.y, 20, 20);
+    ellipse(providence.x, providence.y, 20, 20);
+}
+
+function drawMeteorites() {
+    // Clear Canvas
+    clear();
+
+    for (let i=0; i<meteorites.getRowCount(); i++) {
+        // Get the lat/lng of each meteorite
+        const latitude = Number(meteorites.getString(i, 'reclat'));
+        const longitude = Number(meteorites.getString(i, 'reclong'));
+
+        /* Only draw the meteorites if the position is inside the current map bounds. We use a Leaflet mehtod to check if the lat and lng are contained inside the current map. This way we draw just what we are going to see and not everything. See getBounds() in http://lefletjs.com/reference-1.1.0.html */
+        if (myMap.map.getBounds().contains({lat: latitude, lng: longitude})) {
+            const pos = myMap.latLngToPixel(latitude, longitude);
+            // Get the size of the meteorite and map it. 60000000 is the mass of the largest meteorite
+            let size = meteorites.getString(i, 'mass (g)');
+            size = map(size, 558, 60000000, 1, 25) + myMap.zoom();
+            ellipse(pos.x, pos.y, size, size);
+        }
+    }
 }
 
 function drawMeteorites() {
